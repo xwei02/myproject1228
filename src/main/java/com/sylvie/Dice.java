3b27bb010ab491a570181e2f7bf740d59358b6e6
USER <sylvie.wu@hotmail.com>
2021-12-28 13:39:54 +0800

    commit

---
+++ src/main/java/com/sylvie/Dice.java
@@ -0,0 +1,20 @@
+package com.sylvie;
+
+import java.util.Random;
+
+public class Dice {
+    int point = 1;
+
+    public Dice(){
+        point = new Random().nextInt(6)+1;
+
+    }
+    public boolean isMax(){
+        if(point == 6){
+            return  true;
+        }else{
+            return  false;
+        }
+    }
+    }
+
---
+++ src/main/java/com/sylvie/DiceCounter.java
@@ -0,0 +1,30 @@
+package com.sylvie;
+
+public class DiceCounter {
+    public static void main(String[] args) {
+        int[] counters = new int[6];
+        for (int i = 0; i < 10; i++) {
+            Dice dice = new Dice();
+            Dice d2 = new Dice();
+            System.out.println(dice.point);
+            String s = dice.isMax() ? "*" : "";
+            System.out.println(s);
+            /*if(dice.isMax){
+                System.out.println("*");
+            }else{
+                System.out.println();
+            }*/
+            counters[dice.point-1]++;
+        }
+        int max = 0;
+        int point = 0;
+        for (int i = 0; i < 6; i++) {
+            System.out.println((i+1)+" "+counters[i]);
+            if (counters[i]>max){
+                max = counters[i];
+                point= i+1;
+            }
+        }
+        //System.out.println(point+" "+max+" times");
+    }
+}
